4fddc6a0b82dd3dbc1cb03a84a19cac0cafdc7d4
iamprasadraju <iamprasadraju@duck.com>
2026-01-14 20:51:25 +0530

    refactor template syntax

--- src/gen/commands/template.py
+++ src/gen/commands/template.py
@@ -3,57 +3,49 @@ import shutil
 import sys
 from importlib import resources
 
+from gen.commands import list_
 from gen.config import EXTENSION_MAP
 
 working_dir = os.getcwd()
 
 
 def gen_langtemplate(file, extension, flag=None):
-    if file and extension:
-        lang = EXTENSION_MAP.get(extension)
-        filename = file + extension
+    lang = EXTENSION_MAP.get(extension)
 
-        create_path = os.path.join(working_dir, filename)  # Gives absolute path
+    filename = file + extension
 
-        current_dir = os.path.dirname(__file__)  # Gets the parent dir of lib
-        template_name = f"main{extension}"
+    create_path = os.path.join(working_dir, filename)  # Gives absolute path
 
-        try:
-            template_path = resources.files("gen.templates").joinpath(
-                lang, template_name
-            )
-            if not template_path.exists():
-                raise FileNotFoundError("template does not exist.")
-        except FileNotFoundError as e:
-            print(e)
-            sys.exit(1)
+    current_dir = os.path.dirname(__file__)  # Gets the parent dir of lib
+    template_name = f"main{extension}"
 
-        # print(template_path)
-        with open(template_path, "r") as template:
-            # Reads the template (main.*)
-            content = template.read()
-        if flag is None:
-            if os.path.isfile(create_path):  # check weather file exists
-                print("File is already exists")
-            else:
-                with open(create_path, "w") as file:
-                    file.write(content)
-                    print(f"{filename} created!")
+    template_path = resources.files("gen.templates").joinpath(lang, template_name)
+
+    # print(template_path)
+    with open(template_path, "r") as template:
+        # Reads the template (main.*)
+        content = template.read()
+    if flag is None:
+        if os.path.isfile(create_path):  # check weather file exists
+            print("File is already exists")
         else:
-            if flag == "--dryrun":
-                print(content)
-            elif flag == "--overwrite":
-                with open(create_path, "w") as file:
-                    file.write(content)
-                    print(f"{filename} overwrited!")
+            with open(create_path, "w") as file:
+                file.write(content)
+                print(f"{filename} created!")
     else:
-        pass
+        if flag == "--dryrun":
+            print(content)
+        elif flag == "--overwrite":
+            with open(create_path, "w") as file:
+                file.write(content)
+                print(f"{filename} overwrited!")
 
 
 def gen_framtemplate(dir_name, lang, framework):
     try:
         framework_path = resources.files("gen.templates").joinpath(lang, framework)
         if not framework_path.exists():
+            list_.list_framtemplates()
             raise FileNotFoundError(f"{framework} template does not exist.")
     except FileNotFoundError as e:
         print(e)
